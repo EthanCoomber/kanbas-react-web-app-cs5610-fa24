ab5ff67dad974a923173f2e75069a9b15ec25699
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-09-08 15:24:54 -0400

    2.13

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Labs from './Labs';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
 export default function App() {
   return (
     <HashRouter>
       <div>
         <Routes>
+          <Route path="/" element={<Navigate to="Labs" />} />
           <Route path="/Labs/*" element={<Labs />} />
         </Routes>
       </div>
     </HashRouter>
   );
 }
+// export default App;
