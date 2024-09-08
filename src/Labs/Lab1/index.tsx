bc77005bf662059783fdf17f3ca9860ffbe28e2f
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-09-08 15:09:47 -0400

    2.12

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -19,7 +19,6 @@ export default function Lab1() {
       <p id="wd-p-2">This is the first paragraph. The paragraph tag is used to format vertical gaps between long pieces of text like this one.</p>
       <p id="wd-p-3">This is the second paragraph. Even though there is a deliberate white gap between the paragraph above and this paragraph, by default browsers render them as one contiguous piece of text as shown here on the right.</p>
       <p id="wd-p-4">This is the third paragraph. Wrap each paragraph with the paragraph tag to tell browsers to render the gaps.</p>
-
       <div id="wd-lists">
         <h4>List Tags</h4>
         <h5>Ordered List Tag</h5>
@@ -131,43 +130,78 @@ export default function Lab1() {
       <h5>File upload</h5>
       <input id="wd-upload" type="file" />
       <h5 id="wd-radio-buttons">Radio buttons</h5>
-
       <label>Favorite movie genre:</label>
       <br />
-
       <input type="radio" name="radio-genre" id="wd-radio-comedy" />
       <label htmlFor="wd-radio-comedy">Comedy</label>
       <br />
-
       <input type="radio" name="radio-genre" id="wd-radio-drama" />
       <label htmlFor="wd-radio-drama">Drama</label>
       <br />
-
       <input type="radio" name="radio-genre" id="wd-radio-scifi" />
       <label htmlFor="wd-radio-scifi">Science Fiction</label>
       <br />
-
       <input type="radio" name="radio-genre" id="wd-radio-fantasy" />
       <label htmlFor="wd-radio-fantasy">Fantasy</label>
-
       <h5 id="wd-checkboxes">Checkboxes</h5>
       <label>Favorite movie genre:</label>
       <br />
-
       <input type="checkbox" name="check-genre" id="wd-chkbox-comedy" />
       <label htmlFor="wd-chkbox-comedy">Comedy</label>
       <br />
-
       <input type="checkbox" name="check-genre" id="wd-chkbox-drama" />
       <label htmlFor="wd-chkbox-drama">Drama</label>
       <br />
-
       <input type="checkbox" name="check-genre" id="wd-chkbox-scifi" />
       <label htmlFor="wd-chkbox-scifi">Science Fiction</label>
       <br />
-
       <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy" />
       <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
+      <h4 id="wd-dropdowns">Dropdowns</h4>
+      <h5>Select one</h5>
+      <label htmlFor="wd-select-one-genre"> Favorite movie genre: </label>
+      <br />
+      <select id="wd-select-one-genre">
+        <option value="COMEDY">Comedy</option>
+        <option value="DRAMA">Drama</option>
+        <option selected value="SCIFI">
+          Science Fiction
+        </option>
+        <option value="FANTASY">Fantasy</option>
+      </select>
+      <h5>Select many</h5>
+      <label htmlFor="wd-select-many-genre"> Favorite movie genres: </label>
+      <br />
+      <select id="wd-select-many-genre" multiple>
+        <option selected value="COMEDY">
+          Comedy
+        </option>
+        <option value="DRAMA">Drama</option>
+        <option selected value="SCIFI">
+          Science Fiction
+        </option>
+        <option value="FANTASY">Fantasy</option>
+      </select>
+      <h4>Other HTML field types</h4>
+      <label htmlFor="wd-text-fields-email"> Email: </label>
+      <input type="email" placeholder="jdoe@somewhere.com" id="wd-text-fields-email" />
+      <br />
+      <label htmlFor="wd-text-fields-salary-start"> Starting salary:</label>
+      <input type="number" id="wd-text-fields-salary-start" placeholder="1000" value="100000" />
+      <br />
+      <label htmlFor="wd-text-fields-rating"> Rating: </label>
+      <input type="range" id="wd-text-fields-rating" placeholder="Doe" max="5" value="4" />
+      <br />
+      <label htmlFor="wd-text-fields-dob"> Date of birth: </label>
+      <input type="date" id="wd-text-fields-dob" value="2000-01-21" />
+      <br />
+      <h4>Anchor tag</h4>
+      Please
+      <a id="wd-lipsum" href="https://www.lipsum.com">
+        click here
+      </a>
+      to get dummy text
+      <br />
     </div>
   );
 }
