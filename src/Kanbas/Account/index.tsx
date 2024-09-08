edde97bfc9994802f6f126101f82e46585727acc
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-09-08 15:39:25 -0400

    3.2.4

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -1,9 +1,18 @@
 import Signin from './Signin';
+import { Routes, Route, Navigate } from 'react-router';
+import Profile from './Profile';
+import Signup from './Signup';
+
 export default function Account() {
   return (
     <div id="wd-account-screen">
       <h2>Account</h2>
-      <Signin />
+      <Routes>
+        <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
+        <Route path="/Signin" element={<Signin />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
