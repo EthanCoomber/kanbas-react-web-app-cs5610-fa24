c309624de84287fd814c2576eda50321c8f2bd7a
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-09-08 23:14:44 -0400

    3.8

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -8,6 +8,7 @@ export default function AssignmentEditor() {
       <textarea id="wd-description">The assignment is available online Submit a link to the landing page of</textarea>
       <br />
       <table>
+        <br />
         <tr>
           <td align="right" valign="top">
             <label htmlFor="wd-points">Points</label>
@@ -16,6 +17,7 @@ export default function AssignmentEditor() {
             <input id="wd-points" value={100} />
           </td>
         </tr>
+        <br />
         <tr>
           <td align="right" valign="top">
             <label htmlFor="wd-group">Assignment Group</label>
@@ -24,6 +26,7 @@ export default function AssignmentEditor() {
             <input id="wd-group" value={100} />
           </td>
         </tr>
+        <br />
         <tr>
           <td align="right" valign="top">
             <label htmlFor="wd-display-grade-as">Assignment Group</label>
@@ -34,6 +37,7 @@ export default function AssignmentEditor() {
             </select>
           </td>
         </tr>
+        <br />
         <tr>
           <td align="right" valign="top">
             <label htmlFor="wd-submission-type">Submission Type</label>
@@ -44,65 +48,71 @@ export default function AssignmentEditor() {
             </select>
           </td>
         </tr>
+        <br />
         <tr>
-          <td align="right" valign="top">
+          <td align="left" valign="top"></td>
+          <td>
             <label htmlFor="wd-submission-type">Online Entry Options</label>
-            <input type="checkbox" name="check-genre" id="wd-chkbox-comedy" />
-            <label htmlFor="wd-chkbox-comedy">Text Entry</label>
+            <br />
+            <input type="checkbox" name="text-entry" id="wd-text-entry" />
+            <label htmlFor="wd-text-entry">Text Entry</label>
             <br />
 
-            <input type="checkbox" name="check-genre" id="wd-chkbox-drama" />
-            <label htmlFor="wd-chkbox-drama">Website URL</label>
+            <input type="checkbox" name="website-url" id="wd-website-url" />
+            <label htmlFor="wd-website-url">Website URL</label>
             <br />
 
-            <input type="checkbox" name="check-genre" id="wd-chkbox-scifi" />
-            <label htmlFor="wd-chkbox-scifi">Media Recordings</label>
+            <input type="checkbox" name="media-recordings" id="wd-media-recordings" />
+            <label htmlFor="wd-media-recordings">Media Recordings</label>
             <br />
 
-            <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy" />
-            <label htmlFor="wd-chkbox-fantasy">Student Annotation</label>
+            <input type="checkbox" name="student-annotation" id="wd-student-annotation" />
+            <label htmlFor="wd-student-annotation">Student Annotation</label>
             <br />
 
-            <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy" />
-            <label htmlFor="wd-chkbox-fantasy">File Uploads</label>
+            <input type="checkbox" name="file-upload" id="wd-file-upload" />
+            <label htmlFor="wd-file-upload">File Uploads</label>
           </td>
-          {/* <td>
-            <input id="wd-submission-type" value={100} />
-          </td> */}
         </tr>
+        <br />
         <tr>
           <td align="right" valign="top">
+            <label htmlFor="wd-assign-to">Assign</label>
+          </td>
+          <td>
             <label htmlFor="wd-assign-to">Assign to</label>
-          </td>
-          <td>
-            <input id="wd-assign-to" value={100} />
+            <br />
+            <input id="wd-assign-to" value={'Everyone'} />
           </td>
         </tr>
+        <br />
         <tr>
-          <td align="right" valign="top">
+          <td align="right" valign="top"></td>
+          <td>
             <label htmlFor="wd-due-date">Due</label>
-          </td>
-          <td>
-            <input id="wd-due-date" value={100} />
+            <br />
+            <input type="date" id="wd-due-date" value="2024-05-13" />
+            <br />
           </td>
         </tr>
+        <br />
         <tr>
           <td align="right" valign="top">
+            {/* <label htmlFor="wd-available-from">Available from</label> */}
+          </td>
+          <td>
             <label htmlFor="wd-available-from">Available from</label>
+            <br />
+            <input type="date" id="wd-available-from" value="2024-05-13" />
+            <br />
           </td>
           <td>
-            <input id="wd-available-from" value={100} />
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
             <label htmlFor="wd-available-until">Until</label>
-          </td>
-          <td>
-            <input id="wd-available-until" value={100} />
+            <br />
+            <input type="date" id="wd-available-until" value="2024-05-13" />
+            <br />
           </td>
         </tr>
-        {/* Complete on your own */}
       </table>
     </div>
   );
