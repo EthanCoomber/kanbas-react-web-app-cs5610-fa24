8ad95b96abf93e5f02fe8823d9811132905f799c
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-09-08 21:33:06 -0400

    3.3 update

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -5,18 +5,13 @@ export default function Signin() {
       <h3>Sign in</h3>
       <input id="wd-username" placeholder="username" /> <br />
       <input id="wd-password" placeholder="password" type="password" /> <br />
-      <div id="wd-signin-screen">
-        <h3>Sign in</h3>
-        <input id="wd-username" placeholder="username" /> <br />
-        <input id="wd-password" placeholder="password" type="password" /> <br />
-        <Link id="wd-signin-btn" to="/Kanbas/Dashboard">
-          Sign in
-        </Link>
-        <br />
-        <Link id="wd-signup-link" to="/Kanbas/Account/Signup">
-          Sign up
-        </Link>
-      </div>
+      <Link id="wd-signin-btn" to="/Kanbas/Dashboard">
+        Sign in
+      </Link>
+      <br />
+      <Link id="wd-signup-link" to="/Kanbas/Account/Signup">
+        Sign up
+      </Link>
     </div>
   );
 }
--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -95,11 +95,11 @@ export default function Lab1() {
         <h4>Image tag</h4>
         Loading an image from the internet:
         <br />
-        <img id="wd-starship" width="400px" src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+        <img id="wd-starship" alt="link_img" width="400px" src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
         <br />
         Loading a local image:
         <br />
-        <img id="wd-teslabot" src="/images/teslabot.jpg" height="200px" />
+        <img id="wd-teslabot" alt="local_img" src="/images/teslabot.jpg" height="200px" />
       </div>
       <div id="wd-forms">
         <h4>Form Elements</h4>
