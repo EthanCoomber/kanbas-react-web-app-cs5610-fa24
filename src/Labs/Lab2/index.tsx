3534618bae76e7b73ee1638c30d119db60154eea
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-09-15 22:51:52 -0400

    3.3.2

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -3,7 +3,7 @@ import ReactIconsSampler from './ReactIcons';
 
 export default function Lab2() {
   return (
-    <div id="wd-lab2">
+    <div id="wd-lab2" className="container">
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
       <p>Style attribute allows configuring look and feel right on the element. Although it's very convenient it is considered bad practice and you should avoid using the style attribute</p>
