864811570aade32c55a883994d85db92ca0c6af1
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-09-30 12:32:19 -0400

    2.5

---
+++ src/Labs/Lab3/Classes.tsx
@@ -0,0 +1,34 @@
+import './Classes.css';
+export default function Classes() {
+  const color = 'blue';
+  const dangerous = true;
+  const colorBlack = { color: 'black' };
+  const padding10px = { padding: '10px' };
+  const bgBlue = { backgroundColor: 'lightblue', color: 'black', ...padding10px };
+  const bgRed = {
+    backgroundColor: 'lightcoral',
+    ...colorBlack,
+    ...padding10px,
+  };
+
+  return (
+    <div>
+      <h2>Classes</h2>
+      <div className="wd-bg-yellow wd-fg-black wd-padding-10px">Yellow background </div>
+      <div className="wd-bg-blue wd-fg-black wd-padding-10px">Blue background </div>
+      <div className="wd-bg-red wd-fg-black wd-padding-10px">Red background </div>
+      <div className={`wd-bg-${color} wd-fg-black wd-padding-10px`}>Dynamic Blue background</div>
+      <div
+        className={`${dangerous ? 'wd-bg-red' : 'wd-bg-green'}
+                                     wd-fg-black wd-padding-10px`}
+      >
+        Dangerous background
+      </div>
+      <div style={{ backgroundColor: 'lightyellow', color: 'black', padding: '10px' }}>Yellow background</div>
+      <div style={bgRed}> Red background </div>
+      <div style={bgBlue}>Blue background</div>
+
+      <hr />
+    </div>
+  );
+}
