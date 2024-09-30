7eb02f968d73ec325c0268f99f1266b89d0e46ec
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-09-30 12:19:21 -0400

    2.4.14

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -14,6 +14,17 @@ import ArrayIndexAndLength from './ArrayIndexAndLength';
 import AddingAndRemovingToFromArrays from './AddingAndRemovingToFromArrays';
 import ForLoops from './ForLoops';
 import MapFunction from './MapFunction';
+import FindFunction from './FindFunction';
+import FindIndex from './FindIndex';
+import FilterFunction from './FilterFunction';
+import JsonStringify from './JsonStringify';
+import House from './House';
+import TodoItem from './todos/TodoItem';
+import TodoList from './todos/TodoList';
+import Spreading from './Spreading';
+import Destructing from './Destructing';
+import FunctionDestructing from './FunctionDestructing';
+import DestructingImports from './DestructingImports';
 
 export default function Lab3() {
   return (
@@ -35,6 +46,17 @@ export default function Lab3() {
       <AddingAndRemovingToFromArrays />
       <ForLoops />
       <MapFunction />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+      <JsonStringify />
+      <House />
+      <TodoItem />
+      <TodoList />
+      <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
     </div>
   );
 }
