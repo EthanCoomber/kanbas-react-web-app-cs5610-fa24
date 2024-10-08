e93f8b6755273f59b6030e91c23e2b6dd28bd384
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-10-07 21:11:04 -0400

    2.6.3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -29,6 +29,7 @@ import Classes from './Classes';
 import Add from './Add';
 import Square from './Square';
 import Highlight from './Highlight';
+import PathParameters from './PathParameters';
 
 export default function Lab3() {
   return (
@@ -67,6 +68,7 @@ export default function Lab3() {
       <Square>4</Square>
       <hr />
       <Highlight>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.</Highlight>
+      <PathParameters />
     </div>
   );
 }
