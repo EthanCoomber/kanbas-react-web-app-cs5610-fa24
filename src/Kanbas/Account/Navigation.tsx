cb7b4983a7158151a5981fa298a0e3ceb9e7bc56
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-10-11 14:17:23 -0400

    updated sign up navigation

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,14 +1,18 @@
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router';
+
 export default function AccountNavigation() {
+  const { pathname } = useLocation();
+
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <Link to="/Kanbas/Account/Signin" id="wd-course-home-link" className="list-group-item active border border-0">
+      <Link to="/Kanbas/Account/Signin" id="wd-course-home-link" className={`nav-link ${pathname.includes('Signin') ? 'list-group-item active border border-0' : 'list-group-item text-danger border border-0'}`}>
         Sign in
       </Link>
-      <Link to="/Kanbas/Account/Signup" id="wd-course-modules-link" className="list-group-item text-danger border border-0">
+      <Link to="/Kanbas/Account/Signup" id="wd-course-modules-link" className={`nav-link ${pathname.includes('Signup') ? 'list-group-item active border border-0' : 'list-group-item text-danger border border-0'}`}>
         Sign up
       </Link>
-      <Link to="/Kanbas/Account/Profile" id="wd-course-piazza-link" className="list-group-item text-danger border border-0">
+      <Link to="/Kanbas/Account/Profile" id="wd-course-piazza-link" className={`nav-link ${pathname.includes('Profile') ? 'list-group-item active border border-0' : 'list-group-item text-danger border border-0'}`}>
         Profile
       </Link>
     </div>
