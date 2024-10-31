18598de4e4c185a1bb30d51a42d910b14d7ed3ea
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-10-30 21:50:51 -0400

    fixed protections

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -38,23 +38,28 @@ export default function Assignments() {
               .filter((assignment: any) => assignment.course === cid)
               .map((assignment: any) => (
                 <li className="wd-assignment list-group-item p-2 ps-1">
-                  <BsGripVertical size={40} className="me-2 fs-3 mr-2 d-inline-block mb-4" />
                   {currentUser.role === 'FACULTY' && (
-                    <Link className="wd-assignment-link" to={`${assignment._id}`}>
-                      <FaEdit size={30} color="green" className="me-4 d-inline-block mb-4" />
-                    </Link>
+                    <>
+                      <BsGripVertical size={40} className="me-2 fs-3 mr-2 d-inline-block mb-4" />
+                      <Link className="wd-assignment-link" to={`${assignment._id}`}>
+                        <FaEdit size={30} color="green" className="me-4 d-inline-block mb-4" />
+                      </Link>
+                    </>
                   )}
                   <div className="d-inline-block">
                     <strong>{assignment.title}</strong>
                     <br />
                     <span className="text-danger">Multiple Modules</span> | <strong>Not available until</strong> May 6 at 12:00am | <strong>Due</strong> May 13 at 11:59pm | 100pts
                   </div>
-                  <AssignmentControlButtons
-                    assignmentId={assignment._id}
-                    deleteAssignment={(assignmentId) => {
-                      dispatch(deleteAssignment(assignmentId));
-                    }}
-                  />
+
+                  {currentUser.role === 'FACULTY' && (
+                    <AssignmentControlButtons
+                      assignmentId={assignment._id}
+                      deleteAssignment={(assignmentId) => {
+                        dispatch(deleteAssignment(assignmentId));
+                      }}
+                    />
+                  )}
                 </li>
               ))}
           </ul>
