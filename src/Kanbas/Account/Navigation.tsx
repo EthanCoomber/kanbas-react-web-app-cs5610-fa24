70f5796148ef3ae8ca607e375b2fbb0a3dc5b7f5
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-11-26 14:59:41 -0500

    profile links improved

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,12 +1,10 @@
-import { Link } from 'react-router-dom';
-import { useLocation } from 'react-router';
+import { Link, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
 export default function AccountNavigation() {
-  const { pathname } = useLocation();
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ['Profile'] : ['Signin', 'Signup'];
-
+  const active = (path: string) => (pathname.includes(path) ? 'active' : '');
+  const { pathname } = useLocation();
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
       {links.map((link) => (
@@ -14,6 +12,12 @@ export default function AccountNavigation() {
           {link}
         </Link>
       ))}
+      {currentUser && currentUser.role === 'ADMIN' && (
+        <Link to={`/Kanbas/Account/Users`} id="wd-course-home-link" className={`nav-link ${pathname.includes('Users') ? 'list-group-item active border border-0' : 'list-group-item text-danger border border-0'}`}>
+          {' '}
+          Users{' '}
+        </Link>
+      )}
     </div>
   );
 }
