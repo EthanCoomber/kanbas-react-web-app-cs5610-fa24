16bc8e68344cc6578e2cc3e286795bd11336d271
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-11-27 00:01:45 -0500

    3.6.3

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -44,6 +44,21 @@ export const createCourse = async (course: any) => {
   return data;
 };
 
+export const findUsersByRole = async (role: string) => {
+  const response = await axios.get(`${USERS_API}?role=${role}`);
+  return response.data;
+};
+
+export const findUsersByPartialName = async (name: string) => {
+  const response = await axios.get(`${USERS_API}?name=${name}`);
+  return response.data;
+};
+
+export const findUserById = async (id: string) => {
+  const response = await axios.get(`${USERS_API}/${id}`);
+  return response.data;
+};
+
 // export const findMyCourses = async () => {
 //   console.log('finding courses');
 //   const { data } = await axiosWithCredentials.get(`${USERS_API}/current/courses`);
