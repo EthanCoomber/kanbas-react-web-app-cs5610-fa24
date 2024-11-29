ac9cafba4ea62a5c26c0dd16673fdd24dd85bbd3
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-11-29 16:34:48 -0500

    5.2.5

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -6,14 +6,16 @@ export default function AccountNavigation() {
   const active = (path: string) => (pathname.includes(path) ? 'active' : '');
   const { pathname } = useLocation();
   return (
-    <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
+    <div id="wd-account-navigation" className="list-group">
       {links.map((link) => (
-        <Link to={`/Kanbas/Account/${link}`} id="wd-course-home-link" className={`nav-link ${pathname.includes(link) ? 'list-group-item active border border-0' : 'list-group-item text-danger border border-0'}`}>
-          {link}
+        <Link key={link} to={`/Kanbas/Account/${link}`} className={`list-group-item ${active(link)}`}>
+          {' '}
+          {link}{' '}
         </Link>
       ))}
       {currentUser && currentUser.role === 'ADMIN' && (
-        <Link to={`/Kanbas/Account/Users`} id="wd-course-home-link" className={`nav-link ${pathname.includes('Users') ? 'list-group-item active border border-0' : 'list-group-item text-danger border border-0'}`}>
+        <Link to={`/Kanbas/Account/Users`} className={`list-group-item ${active('Users')}`}>
+          {' '}
           Users{' '}
         </Link>
       )}
--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,24 +1,17 @@
-import { Link, useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-export default function AccountNavigation() {
-  const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const links = currentUser ? ['Profile'] : ['Signin', 'Signup'];
-  const active = (path: string) => (pathname.includes(path) ? 'active' : '');
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router';
+
+export default function CoursesNavigation() {
   const { pathname } = useLocation();
+  const links = ['Home', 'Modules', 'Piazza', 'Zoom', 'Assignments', 'Quizzes', 'Grades', 'People'];
+
   return (
-    <div id="wd-account-navigation" className="list-group">
+    <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
       {links.map((link) => (
-        <Link key={link} to={`/Kanbas/Account/${link}`} className={`list-group-item ${active(link)}`}>
-          {' '}
-          {link}{' '}
+        <Link to={`${link}`} key={link} id={`wd-course-${link}-link`} className={`nav-link ${pathname.includes(link) ? 'list-group-item active border border-0}' : 'list-group-item text-danger border border-0'}`}>
+          {link}
         </Link>
       ))}
-      {currentUser && currentUser.role === 'ADMIN' && (
-        <Link to={`/Kanbas/Account/Users`} className={`list-group-item ${active('Users')}`}>
-          {' '}
-          Users{' '}
-        </Link>
-      )}
     </div>
   );
 }
